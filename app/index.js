2b8ab13b8298b2c6f903e3e3415c8a7bf4757fba
sathibabu <psathibabu@gmail.com>
2017-05-19 12:04:27 +0530

    Add intertoRoman and vice versa

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 var binsearch = require('../lib/binsearch.js');
+var string = require('../lib/strings');
 
 let A = [11,13,14,15,23,45,67,89];
 let B = [2,6,7];
@@ -7,4 +8,4 @@ let C = [];
 //C = merge.merge(A,B,C,0,0,A.length,B.length);
 //console.log(merge.merge(A,B,C,0,0,A.length,B.length));
 //console.log(binsearch.merge(A,B));
-console.log(binsearch.findRange(A,23));
+console.log(string.romanToInt('IV'));
--- tests/stringtest.js
+++ tests/stringtest.js
@@ -21,6 +21,47 @@ describe('Integer to Roman',function () {
 
 	it('besic match',function(){
 		
-		expect(string.intToRoman(100)).to.equal('M');
+		expect(string.intToRoman(11)).to.equal('XI');
+		expect(string.intToRoman(9)).to.equal('IX');
+		expect(string.intToRoman(1)).to.equal('I');
+		expect(string.intToRoman(5)).to.equal('V');
+		expect(string.intToRoman(4)).to.equal('IV');
+
 	});
+
+	it('test from internet',function(){
+		
+		expect(string.intToRoman(49)).to.equal('XLIX');
+		expect(string.intToRoman(99)).to.equal('XCIX');
+		expect(string.intToRoman(124)).to.equal('CXXIV');
+		expect(string.intToRoman(56)).to.equal('LVI');
+		expect(string.intToRoman(1024)).to.equal('MXXIV');
+
+	});
+
+
+});
+
+
+describe('romanToNumber',function(){
+    it('basic test',function(){
+    	expect(string.romanToInt('IV')).to.equal(4);
+    	expect(string.romanToInt('MXXIV')).to.equal(1024);
+    	expect(string.romanToInt('LVI')).to.equal(56);
+    	expect(string.romanToInt('CXXIV')).to.equal(124);
+    	expect(string.romanToInt('XCIX')).to.equal(99);
+    	expect(string.romanToInt('XLIX')).to.equal(49);
+    	expect(string.romanToInt('V')).to.equal(5);
+    	expect(string.romanToInt('I')).to.equal(1);
+    	expect(string.romanToInt('IX')).to.equal(9);
+    	expect(string.romanToInt('XI')).to.equal(11);
+    });
+});
+
+
+describe('atoi',function(){
+    it('basic test',function(){
+
+    	expect(string.atoi('12 2')).to.equal(12);
+    })
 });
