76a32c503ffca99d418c77e7dffdf1b81f57a0e8
sathibabu <psathibabu@gmail.com>
2017-05-19 15:58:03 +0530

    Add revese linkedlist

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 var binsearch = require('../lib/binsearch.js');
 var string = require('../lib/strings');
+var linkedlist = require('../lib/linkedlists');
 
 let A = [11,13,14,15,23,45,67,89];
 let B = [2,6,7];
@@ -8,4 +9,13 @@ let C = [];
 //C = merge.merge(A,B,C,0,0,A.length,B.length);
 //console.log(merge.merge(A,B,C,0,0,A.length,B.length));
 //console.log(binsearch.merge(A,B));
-console.log(string.romanToInt('IV'));
+
+let list = new linkedlist();
+    	
+list.addToList(5);
+list.addToList(7);
+list.addToList(8);
+list.addToList(12);
+list.addToList(14);
+list.reverseList();
+
--- tests/linkedlists.js
+++ tests/linkedlists.js
@@ -13,18 +13,63 @@ describe('LinkedLists',function(){
     	list.addToList(14);
     	expect(list.printList().join(' ')).to.equal('5 7 8 12 14');
 
-    })
+    });
 
     it('tests retrival of elements with single element',function(){
     	let list = new linkedlist();
     	list.addToList(5);
     	expect(list.printList().join(' ')).to.equal('5');
 
-    })
+    });
 
     it('tests printList with out any elements',function(){
     	let list = new linkedlist();
     	expect(list.printList().join(' ')).to.equal('');
 
-    })  
+    });
+
+   
 });
+
+
+
+describe('LinkedLists Reverse',function(){
+	it('Reverse list with a generic case',function(){
+        let list = new linkedlist();
+    	
+    	list.addToList(5);
+    	list.addToList(7);
+    	list.addToList(8);
+    	list.addToList(12);
+    	list.addToList(14);
+    	list.reverseList();
+    	expect(list.printList().join(' ')).to.equal('14 12 8 7 5');
+	});
+
+
+	it('Reverse list with a single node',function(){
+        let list = new linkedlist();
+    	
+    	list.addToList(5);
+    	list.reverseList();
+    	expect(list.printList().join(' ')).to.equal('5');
+	});
+
+	it('Reverse list with no node',function(){
+        let list = new linkedlist();
+    	
+    	list.reverseList();
+    	expect(list.printList().join(' ')).to.equal('');
+	});
+
+
+   it('Reverse list with two nodes',function(){
+        let list = new linkedlist();
+    	
+    	list.addToList(5);
+    	list.addToList(6);
+    	list.reverseList();
+    	expect(list.printList().join(' ')).to.equal('6 5');
+	});
+
+})
