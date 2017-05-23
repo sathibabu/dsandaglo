51ddb68e442582b4261f341cf2ad0d7bc1a35b65
sathibabu <psathibabu@gmail.com>
2017-05-23 08:28:11 +0530

    rotate array added

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 var array = require('../lib/arrays');
 
 
-let A = [[1,2,3],[4,5,6],[7,8,9]];
-expect(array.printZigZag(A,3,3).join(' ')).to.equal('1 2 3 6 5 4 7 8 9');
-console.log(array.printSpiral(A,3,3));
+ let A = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]];
+//expect(array.printZigZag(A,3,3).join(' ')).to.equal('1 2 3 6 5 4 7 8 9');
+console.log(array.rotateArray(A,4));
 
 
--- tests/arrays.js
+++ tests/arrays.js
@@ -104,3 +104,15 @@ describe('Fill Spiral Matrix for given N',function(){
     
 
 });
+
+
+describe('Rotate Matrix',function(){
+  it('3X3',function(){
+      let A = [[1,2,3],[4,5,6],[7,8,9]];
+      expect(array.rotateArray(A,3).join(' ')).to.equal('7,4,1 8,5,2 9,6,3');
+  });
+  it('4X4',function(){
+      let A = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]];
+      expect(array.rotateArray(A,4).join(' ')).to.equal('13,9,5,1 14,10,6,2 15,11,7,3 16,12,8,4');
+  });
+});
